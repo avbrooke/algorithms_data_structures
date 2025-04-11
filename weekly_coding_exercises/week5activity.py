fd3032c7bea1f502863142b1df9ec178df14f067
Alexandra Brooke <alexandravbrooke@gmail.com>
2025-04-11 17:15:10 +0100

    w5l4 coding activity part2

--- weekly_coding_exercises/week5activity.py
+++ weekly_coding_exercises/week5activity.py
@@ -20,6 +20,17 @@ simple_cipher= {'a': 'z', 'b': 'y', 'c': 'x', 'd': 'w', 'e': 'v', 'y': 'b', 'z':
 print(encrypt_message("hello", simple_cipher))
 
 
+shopping_lists = [
+    ["milk", "bread", "eggs", "apples"],
+    ["bread", "eggs", "bananas", "apples"],
+    ["milk", "bread", "apples"]
+]
 
+def find_common_items(lists):
+    set1 = set(lists[0])
+    set2 = set(lists[1])
+    set3 = set(lists[2])
+    result = set1.intersection(set2, set3)
+    return result
 
-
+print(find_common_items(shopping_lists))
