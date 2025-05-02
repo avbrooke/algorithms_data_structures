900737ea8ad984da8cb2d6a2ca352ed4b1395150
Alexandra Brooke <alexandravbrooke@gmail.com>
2025-05-02 23:56:59 +0100

    completed and tested q1

--- summative_assignment/question_1.py
+++ summative_assignment/question_1.py
@@ -1,2 +1,15 @@
 def data_smoothing(data, size):
-    pass
+    if size < 1 or size > len(data):
+        raise ValueError("Invalid size: must be greater than or equal to 1, and less than or equal to length of data ")
+
+    smoothed_data = []
+    for d in range(len(data)):
+        if d < size - 1:
+            smoothed_data.append(None)
+        else:
+            total = 0.0
+            for s in range(size):
+                total += data[d-s]
+            smoothed_data.append(total/size)
+
+    return smoothed_data
