453e7272d66f150c1ca4d5cff57364958251237f
Alexandra Brooke <alexandravbrooke@gmail.com>
2025-05-04 18:04:31 +0100

    q3 completed

--- summative_assignment/question_3.py
+++ summative_assignment/question_3.py
@@ -1,2 +1,23 @@
 def get_words_indices(text):
-    pass
+    indices = {}
+    idx = 0
+
+    while idx < len(text):
+        if text[idx] == " ":
+            idx +=1
+            continue
+
+        word_start = idx
+        while idx < len(text) and text[idx] != " ":
+            idx += 1
+        word = text[word_start:idx].lower()
+
+        if word in indices:
+            indices[word].append(word_start)
+        else:
+            indices[word] = [word_start]
+
+    return indices
+
+sampleText = "We can only see a short distance ahead" + " but we can see plenty there that needs" + " to be done"
+print(get_words_indices(sampleText))
