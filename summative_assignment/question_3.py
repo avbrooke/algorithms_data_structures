80ef8c3d2bac11c53b989f9df2b10a751b275969
Alexandra Brooke <alexandravbrooke@gmail.com>
2025-05-06 00:53:22 +0100

    documentation added

--- summative_assignment/question_3.py
+++ summative_assignment/question_3.py
@@ -1,6 +1,17 @@
 def get_words_indices(text):
-    indices = {}
-    idx = 0
+    """
+    Returns a dictionary that maps each given word to a list of starting indices where that word occurs.
+
+    Words are case-insensitive.
+    Words are assumed to be separated by at least one space, and have no punctuation.
+    Args:
+        text (str): The input string with words separated by space(s)
+    Returns:
+        dict: A dictionary in which each key is a lowercase word and the value is a list of starting
+        indices where the word appears.
+    """
+    indices = {} # Dictionary for holding words and list of positions
+    idx = 0 # Current index in the text
 
     while idx < len(text):
         if text[idx] == " ":
